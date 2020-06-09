8148ef40ae4047a7c0f543f8ca6d7354fc0e44dc
lakto69 <leite.am@gmail.com>
2020-06-09 15:06:58 -0300

    Alterações diversas.

--- avulsos/mypolygon.py
+++ avulsos/mypolygon.py
@@ -1,10 +1,36 @@
 import turtle
+import math
+
 bob = turtle.Turtle()
 
-def polygon(t, n, length):
-    angle = 360 / n
+
+def polyline(t, n, length, angle):
+    """Desenha n segmentos de linha com o comprimento e o ângulo (em graus) entre eles.\
+    t é uma tartaruga.
+    """
     for i in range(n):
         t.fd(length)
         t.lt(angle)
-#rrr
-polygon(bob, 10, 10)
+
+
+def polygon(t, n, length):
+    angle = 360 / n
+    polyline(t, n, length, angle)
+
+
+def circle(t, r):
+    arc(t, r, 360)
+
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
+    polyline(t, n, step_length, step_angle)
+
+
+# circle(bob, 40)
+arc(bob, 40, 30)
+
+turtle.mainloop()
